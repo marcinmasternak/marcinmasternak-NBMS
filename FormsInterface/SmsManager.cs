6874c7992268ac59b0fc01018fe2b622f5bcb811
Zenon <marcin.masternak@gmail.com>
2020-12-01 12:06:48 +0000

    second

--- FormsInterface/SmsManager.cs
+++ FormsInterface/SmsManager.cs
@@ -19,7 +19,7 @@ namespace FormsInterface
         List<string> MessageFieldsLong = new List<string> { "Message type:         ", "\nMessage header:    ",
                                                             "\nSender:                        ",
                                                             "\n------------------------- Message Text ----------------------:\n\n" };
-        //test commit
+        //test commitrtyrteyertyerty
         List<string> MessageContent = new List<string>();
         public SmsManager(string _header, string _body, Form1 myForm)
         {
